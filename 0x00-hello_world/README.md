d1aa182b72a749fdec93b948e52f85968206a276
root <iobutho17@alustudent.com>
2020-09-16 08:52:41 +0000

    Added all the task files for the project

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,3 @@
+0x00-hello world
+This project directory contains the Hello World Projects
+
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("This is a C file, Name: %c made 0:%d", 'C', 23);
+	return (0);
+}
+
