b5efad16f546989a0cc88333fb93a7f05e60bad0
root <iobutho17@alustudent.com>
2020-09-22 11:25:03 +0000

    added task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - sums nultplies of 3 or 5
+ *
+ * Description: multiples between 0 and 1024
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int start, end, sum;
+
+	end = 1024;
+	sum = 0;
+
+	for (start = 0; start < end; start++)
+	{
+		if ((start % 3 == 0) || (start % 5 == 0))
+		{
+			sum = sum + start;
+		} else
+		{
+			continue;
+		}
+	}
+
+	printf("%d", sum);
+	printf("\n");
+
+	return (0);
+}
