befbb7a962f97b24da61c2a3424980809d57c388
root <iobutho17@alustudent.com>
2020-09-22 06:04:10 +0000

    Updated task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,24 @@
 #include "holberton.h"
 /**
  * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
  *
- * Description: 
+ * Description: prints lowercase letters
  * Return: Always(0) Success
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
+
 	else if (c >= 'A' && c <= 'Z')
 		return (0);
-	else
+
+	else if (c == '\0')
 		return (1);
+
+	else
+		return (0);
+
 	return (0);
 }
