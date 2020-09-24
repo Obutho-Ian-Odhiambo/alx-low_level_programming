a97fae5fbdf6371e144403dfb52063e0c9a58c20
root <iobutho17@alustudent.com>
2020-09-24 11:29:28 +0000

    Updated task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,16 +7,16 @@
  */
 void main(void)
 {
-	long long int n , i;
+	long long int n, i;
 
 	n = 612852475143;
 
-	for (i = 3; i <= n; i = i+2)
+	for (i = 3; i <= n; i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			n = n/i;
+			n = n / i;
 		}
 	}
-	printf("%lld ", i);
+	printf("%lld", i);
 }
