bb67df4fb8a31cee1d3db54dcc79921cb02c8ae7
root <iobutho17@alustudent.com>
2020-09-28 14:50:54 +0000

    Added task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array size n
+ * @a: first parameter
+ * @n: second parameter
+ *
+ * Description: n may vary occasionaly
+ * Return: Always(0) Success
+ */
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; a[i] != '\0'; ++i)
+	{
+		if ( i == (n - 1))
+		{
+			printf("%d", a[i]);
+			break;
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	printf("\n");
+}
