468ad04b70b2469f05dd368ddf3a82c133782739
root <iobutho17@alustudent.com>
2020-10-01 16:04:54 +0000

    Updated task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,14 @@
 char *string_toupper(char *s)
 {
 	int x = 0;
-	char *str = s;
 
 	while (s[x] != '\0')
 	{
 		if ((s[x] >= 'a') && (s[x] <= 'z'))
 		{
 			s[x] = (s[x] - 32);
-			*str = s[x];
 		}
 		x++;
 	}
-	return (str);
+	return (s);
 }
