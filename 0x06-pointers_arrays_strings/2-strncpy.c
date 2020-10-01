4b8f0fae6bbdd03d7a2d313ca191c9674b385130
root <iobutho17@alustudent.com>
2020-10-01 16:16:50 +0000

    Updated 10

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *copy = dest;
+	int i = 0;
 
-	while (*src != '\0')
+	while ((*src != '\0') && (i <= n))
 	{
 		*dest = *src;
 		dest++;
 		src++;
-		n += 1;
+		i += 1;
 	}
 	*dest = '\0';
 	return (copy);
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -7,3 +7,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *);
+void print_number(int n);
