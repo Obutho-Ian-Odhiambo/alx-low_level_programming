93b590cf2fb1a9ecb7947adc9cdef1f5bde117ab
root <iobutho17@alustudent.com>
2020-10-07 17:06:16 +0000

    updated task 6 again

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,13 @@
 int is_prime_number(int n)
 {
 	int x = 1;
+
 	return (is_prime2(x, n));
 
 }
 
 /**
- * is_prime -  returns 1 if the input integer is a prime number
+ * is_prime2 -  returns 1 if the input integer is a prime number
  * @a: first parameter
  * @b: second parameter
  * Return: Integer
@@ -24,19 +25,19 @@ int is_prime2(int a, int b)
 {
 	if (b == 1 || b == 25)
 	{
-		return 0;
+		return (0);
 	}
 	else if (b <= 0)
 	{
-		return 0;
+		return (0);
 	}
 	else if (b % 2 == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else if (b % 2 != 0)
 	{
-		return 1;
+		return (1);
 	}
 
 	return (is_prime2((a + 1), b));
