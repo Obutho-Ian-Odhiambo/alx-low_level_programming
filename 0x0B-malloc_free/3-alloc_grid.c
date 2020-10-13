4182e8e556dbf4798d8ef0eaefb2f3e986226526
root <iobutho17@alustudent.com>
2020-10-13 14:25:44 +0000

    Updated task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,16 @@ int **alloc_grid(int width, int height)
 	int **array;
 	int i;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+
 	array = (int **) malloc(height * sizeof(int *));
+
+
+	if (array == NULL)
+		return (NULL);
+
 
 	for (i = 0; i < height; i++)
 	{
