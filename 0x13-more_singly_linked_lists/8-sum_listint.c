d1156b3048b468bb690fb15934a764d136af4f53
Obutho-Ian-Odhiambo <iobutho17@alustudent.com>
2020-11-04 20:49:14 +0300

    tried to fix 9

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -14,7 +14,7 @@ int sum_listint(listint_t *head)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
+		return (0);
 
 	temp = head;
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,45 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *new;
-	unsigned int i = 0;
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int i;
 
-	temp = *head;
-	new = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-	if (new == NULL || head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	new_node->n = n;
+	new_node->next = NULL;
+	temp = *head;
+	i = 0;
+
+	if (*head == NULL && index > 0)
 	{
-		new->n = n;
-		new->next = temp;
-		*head = new;
-		return (new);
+		free(new_node);
+		return (NULL);
 	}
 
-	while (i < idx - 1)
+	if (index == 0)
 	{
-		if (temp == NULL || temp->next == NULL)
-			return (NULL);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (i < index - 1)
+	{
 		temp = temp->next;
+		if (temp == NULL && index - i > 0)
+		{
+			free(new_node);
+			return (NULL);
+		}
 		i++;
 	}
 
-	new->n = n;
-	new->next = temp->next;
-	temp->next = new;
-	return (new);
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
